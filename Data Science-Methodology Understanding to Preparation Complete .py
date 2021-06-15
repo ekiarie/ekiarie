7121520e9bccf3eed0b5bfb638c908394755325c
Erickson Kiarie <77984369+ekiarie@users.noreply.github.com>
2021-06-15 16:26:17 +0300

    Update Data Science-Methodology Understanding to Preparation Complete .py
    Predicting cuisines using ingredients data obtained from scrapped information from websites.

--- Data Science-Methodology Understanding to Preparation Complete .py
+++ Data Science-Methodology Understanding to Preparation Complete .py
@@ -3,46 +3,36 @@
 
 # In[1]:
 
-
 get_ipython().system('python -V')
-
 
 # Download the library and dependencies that we will need to run this lab.
 
 # In[2]:
-
 
 import pandas as pd # import library to read data into dataframe
 pd.set_option('display.max_columns', None)
 import numpy as np # import numpy library
 import re # import library for regular expression
 
-
 # Download the data from the IBM server and read it into a pandas dataframe.
 
 # In[3]:
-
 
 recipes = pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0103EN-SkillsNetwork/labs/Module%202/recipes.csv")
 
 print("Data read into dataframe!") # takes about 30 seconds
 
-
 # Show the first few rows.
 
 # In[4]:
 
-
 recipes.head()
-
 
 # Get the dimensions of the dataframe.
 
 # In[5]:
 
-
 recipes.shape
-
 
 # So our dataset consists of 57,691 recipes. Each row represents a recipe, and for each recipe, the corresponding cuisine is documented as well as whether 384 ingredients exist in the recipe or not, beginning with almond and ending with zucchini.
 # 
@@ -56,13 +46,11 @@ recipes.shape
 
 # In[6]:
 
-
 ingredients = list(recipes.columns.values)
 
 print([match.group(0) for ingredient in ingredients for match in [(re.compile(".*(rice).*")).search(ingredient)] if match])
 print([match.group(0) for ingredient in ingredients for match in [(re.compile(".*(wasabi).*")).search(ingredient)] if match])
 print([match.group(0) for ingredient in ingredients for match in [(re.compile(".*(soy).*")).search(ingredient)] if match])
-
 
 # Yes, they do!
 # 
@@ -73,9 +61,7 @@ print([match.group(0) for ingredient in ingredients for match in [(re.compile(".
 
 # In[7]:
 
-
 recipes["country"].value_counts() # frequency table
-
 
 # By looking at the above table, we can make the following observations:
 # 
@@ -90,26 +76,21 @@ recipes["country"].value_counts() # frequency table
 
 # In[8]:
 
-
 column_names = recipes.columns.values
 column_names[0] = "cuisine"
 recipes.columns = column_names
 
 recipes
 
-
 # Make all the cuisine names lowercase.
 
 # In[9]:
 
-
 recipes["cuisine"] = recipes["cuisine"].str.lower()
-
 
 # Make the cuisine names consistent.
 
 # In[10]:
-
 
 recipes.loc[recipes["cuisine"] == "austria", "cuisine"] = "austrian"
 recipes.loc[recipes["cuisine"] == "belgium", "cuisine"] = "belgian"
@@ -142,11 +123,9 @@ recipes.loc[recipes["cuisine"] == "irish", "cuisine"] = "uk-and-irish"
 
 recipes
 
-
 # Remove cuisines with < 50 recipes.
 
 # In[11]:
-
 
 # get list of cuisines to keep
 recipes_counts = recipes["cuisine"].value_counts()
@@ -154,9 +133,7 @@ cuisines_indices = recipes_counts > 50
 
 cuisines_to_keep = list(np.array(recipes_counts.index.values)[np.array(cuisines_indices)])
 
-
 # In[12]:
-
 
 rows_before = recipes.shape[0] # number of rows of original dataframe
 print("Number of rows of original dataframe is {}.".format(rows_before))
@@ -168,26 +145,20 @@ print("Number of rows of processed dataframe is {}.".format(rows_after))
 
 print("{} rows removed!".format(rows_before - rows_after))
 
-
 # Convert all Yes's to 1's and the No's to 0's
 
 # In[13]:
 
-
 recipes = recipes.replace(to_replace="Yes", value=1)
 recipes = recipes.replace(to_replace="No", value=0)
-
 
 # Run the following cell to get the recipes that contain rice and soy and wasabi and seaweed.
 
 # In[14]:
 
-
 recipes.head()
 
-
 # In[15]:
-
 
 check_recipes = recipes.loc[
     (recipes["rice"] == 1) &
@@ -198,7 +169,6 @@ check_recipes = recipes.loc[
 
 check_recipes
 
-
 # Based on the results of the above code, can we classify all recipes that contain rice and soy and wasabi and seaweed as Japanese recipes? Why?
 
 # No. Other Asian countries have such engredients
@@ -207,13 +177,10 @@ check_recipes
 
 # In[16]:
 
-
 # sum each column
 ing = recipes.iloc[:, 1:].sum(axis=0)
 
-
 # In[17]:
-
 
 # define each column as a pandas series
 ingredient = pd.Series(ing.index.values, index = np.arange(len(ing)))
@@ -224,17 +191,14 @@ ing_df = pd.DataFrame(dict(ingredient = ingredient, count = count))
 ing_df = ing_df[["ingredient", "count"]]
 print(ing_df.to_string())
 
-
 # Now we have a dataframe of ingredients and their total counts across all recipes. Let's sort this dataframe in descending order.
 
 # In[18]:
-
 
 ing_df.sort_values(["count"], ascending=False, inplace=True)
 ing_df.reset_index(inplace=True, drop=True)
 
 print(ing_df)
-
 
 # However, note that there is a problem with the above table. There are ~40,000 American recipes in our dataset, which means that the data is biased towards American ingredients.
 # 
@@ -245,10 +209,8 @@ print(ing_df)
 
 # In[19]:
 
-
 cuisines = recipes.groupby("cuisine").mean()
 cuisines.head()
-
 
 # As shown above, we have just created a dataframe where each row is a cuisine and each column (except for the first column) is an ingredient, and the row values represent the percentage of each ingredient in the corresponding cuisine.
 # 
@@ -260,7 +222,6 @@ cuisines.head()
 # Let's print out the profile for each cuisine by displaying the top four ingredients in each cuisine.
 
 # In[20]:
-
 
 num_ingredients = 4 # define number of top ingredients to print
 
@@ -278,7 +239,6 @@ def print_top_ingredients(row):
 # apply function to cuisines dataframe
 create_cuisines_profiles = cuisines.apply(print_top_ingredients, axis=1)
 
-
 # At this point, we feel that we have understood the data well and the data is ready and is in the right format for modeling!
 
 # ### Data Modelling Stage
@@ -286,7 +246,6 @@ create_cuisines_profiles = cuisines.apply(print_top_ingredients, axis=1)
 # Download and install more libraries and dependencies to build decision trees.
 
 # In[21]:
-
 
 # import decision trees scikit-learn libraries
 get_ipython().run_line_magic('matplotlib', 'inline')
@@ -302,14 +261,11 @@ from sklearn.tree import export_graphviz
 
 import itertools
 
-
 # Check the data again!
 
 # In[22]:
 
-
 recipes.head()
-
 
 # #### [bamboo_tree] Only Asian and Indian Cuisines
 # 
@@ -318,7 +274,6 @@ recipes.head()
 # Let's build our decision tree using the data pertaining to the Asian and Indian cuisines and name our decision tree *bamboo_tree*.
 
 # In[23]:
-
 
 # select subset of cuisines
 asian_indian_recipes = recipes[recipes.cuisine.isin(["korean", "japanese", "chinese", "thai", "indian"])]
@@ -330,11 +285,9 @@ bamboo_tree.fit(ingredients, cuisines)
 
 print("Decision tree model saved to bamboo_tree!")
 
-
 # Let's plot the decision tree and examine how it looks like.
 
 # In[24]:
-
 
 export_graphviz(bamboo_tree,
                 feature_names=list(ingredients.columns.values),
@@ -350,7 +303,6 @@ export_graphviz(bamboo_tree,
 with open("bamboo_tree.dot") as bamboo_tree_image:
     bamboo_tree_graph = bamboo_tree_image.read()
 graphviz.Source(bamboo_tree_graph)
-
 
 # The decision tree learned:
 # 
@@ -369,31 +321,24 @@ graphviz.Source(bamboo_tree_graph)
 
 # In[25]:
 
-
 bamboo = recipes[recipes.cuisine.isin(["korean", "japanese", "chinese", "thai", "indian"])]
-
 
 # Let's see how many recipes exist for each cuisine.
 
 # In[26]:
 
-
 bamboo["cuisine"].value_counts()
-
 
 # Let's remove 30 recipes from each cuisine to use as the test set, and let's name this test set **bamboo_test**.
 
 # In[27]:
 
-
 # set sample size
 sample_n = 30
-
 
 # Create a dataframe containing 30 recipes from each cuisine, selected randomly.
 
 # In[43]:
-
 
 # take 30 recipes from each cuisine
 import random
@@ -403,20 +348,16 @@ bamboo_test = bamboo.groupby("cuisine", group_keys=False).apply(lambda x: x.samp
 bamboo_test_ingredients = bamboo_test.iloc[:,1:] # ingredients
 bamboo_test_cuisines = bamboo_test["cuisine"] # corresponding cuisines or labels
 
-
 # Check that there are 30 recipes for each cuisine.
 
 # In[44]:
 
-
 # check that we have 30 recipes from each cuisine
 bamboo_test["cuisine"].value_counts()
-
 
 # Next, let's create the training set by removing the test set from the **bamboo dataset**, and let's call the training set **bamboo_train**.
 
 # In[45]:
-
 
 bamboo_test_index = bamboo.index.isin(bamboo_test.index)
 bamboo_train = bamboo[~bamboo_test_index]
@@ -424,30 +365,24 @@ bamboo_train = bamboo[~bamboo_test_index]
 bamboo_train_ingredients = bamboo_train.iloc[:,1:] # ingredients
 bamboo_train_cuisines = bamboo_train["cuisine"] # corresponding cuisines or labels
 
-
 # Check that there are 30 *fewer* recipes now for each cuisine.
 
 # In[46]:
 
-
 bamboo_train["cuisine"].value_counts()
-
 
 # Let's build the decision tree using the training set, **bamboo_train**, and name the generated tree **bamboo_train_tree** for prediction.
 
 # In[47]:
-
 
 bamboo_train_tree = tree.DecisionTreeClassifier(max_depth=15)
 bamboo_train_tree.fit(bamboo_train_ingredients, bamboo_train_cuisines)
 
 print("Decision tree model saved to bamboo_train_tree!")
 
-
 # Let's plot the decision tree and explore it.
 
 # In[48]:
-
 
 export_graphviz(bamboo_train_tree,
                 feature_names=list(bamboo_train_ingredients.columns.values),
@@ -464,21 +399,17 @@ with open("bamboo_train_tree.dot") as bamboo_train_tree_image:
     bamboo_train_tree_graph = bamboo_train_tree_image.read()
 graphviz.Source(bamboo_train_tree_graph)
 
-
 # Now that we defined our tree to be deeper, more decision nodes are generated.
 
 # In[49]:
 
-
 bamboo_pred_cuisines = bamboo_train_tree.predict(bamboo_test_ingredients)
-
 
 # To quantify how well the decision tree is able to determine the cuisine of each recipe correctly, we will create a confusion matrix which presents a nice summary on how many recipes from each cuisine are correctly classified. It also sheds some light on what cuisines are being confused with what other cuisines.
 
 # So let's go ahead and create the confusion matrix for how well the decision tree is able to correctly classify the recipes in **bamboo_test**.
 
 # In[51]:
-
 
 test_cuisines = np.unique(bamboo_test_cuisines)
 bamboo_confusion_matrix = confusion_matrix(bamboo_test_cuisines, bamboo_pred_cuisines, test_cuisines)
@@ -510,7 +441,6 @@ plt.xlabel('Predicted label')
 
 plt.show()
 
-
 # After running the above code, you should get a confusion matrix similar to the following:
 
 # The rows represent the actual cuisines from the dataset and the columns represent the predicted ones. Each row should sum to 100%. According to this confusion matrix, we make the following observations:
@@ -522,7 +452,3 @@ plt.show()
 # **Please note** that because decision trees are created using random sampling of the datapoints in the training set, then you may not get the same results every time you create the decision tree even using the same training set. The performance should still be comparable though! So don't worry if you get slightly different numbers in your confusion matrix than the ones shown above.
 
 # In[ ]:
-
-
-
-
